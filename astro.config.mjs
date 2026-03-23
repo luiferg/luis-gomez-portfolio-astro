678dc3f77188d330fb97d5fdd6aa24af7f63f9ec
luiferg <luisfgomezortiz@gmail.com>
2026-03-23 12:34:39 +0100

    added web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   // ...
@@ -8,4 +9,9 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
